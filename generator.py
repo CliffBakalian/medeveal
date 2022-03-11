68fe2cda26d28edba7467f3f787c9ed74a1e8071
cyb <cliffbakalian@gmail.com>
2022-03-11 02:19:58 -0500

    added temp css, basically done generator

--- generator.py
+++ generator.py
@@ -1,5 +1,4 @@
-theme = "my"
-refs=["dist/reveal.css","dist/theme/white.css","plugin/highlight/stackoverflow.css","dist/theme1.css"]
+refs=["dist/reveal.css","dist/theme/white.css","plugin/highlight/stackoverflow.css","theme1.css"]
 
 '''
 #HTML PAge
@@ -7,7 +6,7 @@ refs=["dist/reveal.css","dist/theme/white.css","plugin/highlight/stackoverflow.c
 
 def generate_head(hrefs):
   ##TODO need to make generic
-  title = make_tag("title",[],"CMSC250")+"\n"
+  title = make_tag("title",[],"CMSC250")
   meta_char = '<meta charset="utf-8">\n'  
   meta = '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">\n'
   links = ""
@@ -18,7 +17,7 @@ def generate_head(hrefs):
 def generate_html(hrefs,body):
   head = generate_head(hrefs)
   final_body = generate_reveal_body(body)
-  return make_tag("html",[],head+"\n"+final_body)
+  return make_tag("html",[],head+final_body)
 
 '''
 # COMMON HTTP Attributes
@@ -64,12 +63,12 @@ def make_properties(strings):
 def make_tag(tag,properties=[],body=""):
   properties = make_properties(list(filter(None,properties))) #remove empty strings
   if properties =="":
-    return "<"+tag+">"+body+"</"+tag+">"
+    return "<"+tag+">"+body+"</"+tag+">\n"
   else:
-    return "<"+tag+" "+properties+">"+body+"</"+tag+">"
+    return "<"+tag+" "+properties+">"+body+"</"+tag+">\n"
 
 def generate_shape(shape,properties=[],classes=[],frag=True,frag_idx=-1,frag_type="",data_id=-1,style=""):
-  class_prop = make_class_property([make_fragment_classes(frag,frag_type), theme+'-'+shape]+classes) 
+  class_prop = make_class_property([make_fragment_classes(frag,frag_type), shape]+classes) 
   properties.append(class_prop) 
   properties.append(make_properties([make_fragment_properties(frag_idx,data_id)])) 
   properties.append(make_style_string(style))
@@ -101,7 +100,7 @@ REVEAL JS wrapper
 def generate_reveal_body(body):
   slides_body = make_tag("div",[make_class_property(["reveal"])],make_tag("div",[make_class_property(["slides"])],"\n"+body))
   reveal_info = generate_reveal_plugins()+generate_reveal_script()
-  return make_tag("body",[],slides_body+"\n"+reveal_info)
+  return make_tag("body",[],slides_body+reveal_info)
 
 def generate_reveal_script():
   return make_tag("script",[],generate_reveal_config()+generate_reveal_init())
@@ -135,7 +134,7 @@ plugins: [ RevealHighlight, RevealMarkdown, RevealMath, RevealSearch, RevealZoom
 def generate_reveal_plugins(plugins=["dist/reveal.js","plugin/highlight/highlight.js","plugin/markdown/markdown.js","plugin/math/math.js","plugin/search/search.js","plugin/zoom/zoom.js"]):
   plugs = ""
   for i in plugins:
-    plugs += generate_script(i)+"\n"
+    plugs += generate_script(i)
   return plugs 
 
 '''
@@ -146,7 +145,7 @@ Slide Styles
 
 def generate_roadmap_slide(title,roadmap,colors,shape="diamond",connect=True):
   title_attr =generate_text(title,"h3",[],[],False,-1,"",-1,"") 
-  body = title_attr+"\n"+generate_roadmap(roadmap,colors,shape,connect)
+  body = title_attr+generate_roadmap(roadmap,colors,shape,connect)
   return generate_slide(body,["data-auto-animate","data-auto-animate-unmatched='fade-up'"],"height:600px",[])
 
 def generate_roadmap(text,colors,shape="diamond",connect=True):
@@ -157,15 +156,15 @@ def generate_roadmap(text,colors,shape="diamond",connect=True):
   try:
     for idx,value in enumerate(text):
       if idx != 0 and connect:
-        curr_bullet += generate_shape("rmvline",[],[],True,idx+1,"fade-down",-1,"top:"+str(base)+"px;")+"\n"
-      curr_bullet += generate_shape(shape,[],[],True,idx+1,"fade-down",idx+1,"background:"+colors[idx]+";top:"+str(base+(distance/2))+"px;")+"\n"
-      curr_bullet += generate_text(value,"div",[],[theme+"-rm-data"],True,idx+1,"fade-down",-1,"top:"+str(base+33)+"px;")+"\n\n"
+        curr_bullet += generate_shape("rmvline",[],[],True,idx+1,"fade-down",-1,"top:"+str(base)+"px;")
+      curr_bullet += generate_shape(shape,[],[],True,idx+1,"",idx+1,"background:"+colors[idx]+";top:"+str(base+(distance/2))+"px;")
+      curr_bullet += generate_text(value,"div",[],["rm-data"],True,idx+1,"fade-down",-1,"top:"+str(base+33)+"px;")
       base+=distance
       roadmap+=curr_bullet
       curr_bullet=""
   except IndexError:  
-      curr_bullet += generate_shape(shape,[],[],True,idx+1,"fade-down",idx+1,"background:"+colors[len(text)-1]+";top:"+str(base+(distance/2))+"px;")+"\n"
-      curr_bullet += generate_text(value,"div",[],[theme+"-rm-data"],True,idx+1,"fade-down",-1,"top:"+str(base+33)+"px;")+"\n\n"
+      curr_bullet += generate_shape(shape,[],[],True,idx+1,"",idx+1,"background:"+colors[len(text)-1]+";top:"+str(base+(distance/2))+"px;")
+      curr_bullet += generate_text(value,"div",[],["rm-data"],True,idx+1,"fade-down",-1,"top:"+str(base+33)+"px;")
       roadmap+=curr_bullet
       
   return roadmap
@@ -178,15 +177,63 @@ def generate_title_slide(title,subtitle,img="",title_style="",subtitle_style="",
   attrs = [title_attr,subtitle_attr,img_attr]
   body = ""
   for i in order:
-    body += attrs[i]+"\n"   
+    body += attrs[i]   
   return generate_slide(body,["data-auto-animate","data-auto-animate-unmatched='fade-up'"],"",[])
 
+#DOTS 
+def generate_dots(properties=[],style=[],dnum=3,currdot=1,dcolor="purple",shape="rm-circle",space=20,start=-50):
+  dots=""
+  for i in range(dnum):
+    dotstyle="";
+    if i+1 == currdot:
+      dotstyle+="background:"+dcolor+";"
+    dotstyle+="bottom:"+str((dnum-(i+1))*space+start)+"px;"
+    dots+=generate_shape(shape,properties[:],[],False,-1,"",i+1,dotstyle) 
+  return dots
+
+#TWO COLUMNS
+def generate_split_slide(body1,body2,width1=50,width2=50):
+  lwidth=make_style_string("width:'"+str(width1)+"'%")
+  rwidth=make_style_string("width:'"+str(width2)+"'%")
+  lbody=make_tag("div",[lwidth],body1)
+  rbody=make_tag("div",[rwidth],body2)
+  return make_tag("div",[make_class_property(["container"])],lbody+rbody)
+
+'''
+TIME TO EXPORT
+'''
 # WRITE TO FILE 
 def generate_slides(src_file):
   #TODO actually have to write the parser lol
   rm = (generate_roadmap_slide("sets, functions, relations",["person of the week","countability","set proofs"],["purple","blue","orange"],"rm-diamond",True))
   tit =(generate_title_slide("CMSC250","sets, functions, relations","dist/assets/funcpun.jpeg","","","height:300px",[0,1,2]))
-  return generate_html(refs,tit+"\n"+rm)
+  
+  #slide1 test
+  title_attr = generate_text("slide1","h3",[],[],False,-1,"",-1,"") 
+  dots = generate_dots([],[],5,1,"purple","rm-circle",20,-50)
+  slide1 = generate_slide(title_attr+dots,["data-auto-animate"],"height:600px",[])
+  
+  #slide2 test
+  title_attr = generate_text("slide2","h3",[],[],False,-1,"",-1,"") 
+  dots = generate_dots([],[],5,2,"blue","rm-circle",20,25)
+
+  #slide2.1 test
+  s2_1p = make_tag("p",[],"Hello")
+  slide2_1 = generate_slide(s2_1p,["data-auto-animate"],"",[])
+
+  #slide2.2 test
+  s2_2p = make_tag("p",[],"world")
+
+  slide2_2 = generate_slide(s2_2p,["data-auto-animate"],"",[])
+  
+  slide2=generate_slide(title_attr+dots+slide2_1+slide2_2,["data-auto-animate"],"",[])
+
+  #slide3 test
+  title_attr = generate_text("slide3","h3",[],[],False,-1,"",-1,"") 
+  dots = generate_dots([],[],5,3,"green","rm-circle",20,-50)
+  slide3 = generate_slide(title_attr+dots,["data-auto-animate"],"height:600px",[])
+  
+  return generate_html(refs,tit+rm+slide1+slide2+slide3)
 
 f = open("myslides.html", "w")
 slides_to_make = raw_input("file name: ")
